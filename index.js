379627942d240d2a94e46700aeb2a4b90a25a3dd
pacokwon <haechank@gmail.com>
2020-04-22 12:05:01 +0000

    add - fetch user data from sso server

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const client = require('./utils/sso.js')
 
 const app = express()
+let session = {}
+
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-	res.json({'a': 'b'})
+    res.sendFile('index.html')
+})
+
+app.post('/login', (req, res) => {
+    const { url, state } = client.getLoginParams()
+    session.state = state
+    res.redirect(url)
+})
+
+app.get('/account/login/callback', async (req, res) => {
+    const { code, state } = req.query
+    console.log(code, state)
+    const stateBefore = session.state
+
+    if (stateBefore !== state)
+        throw new Error('TOKEN MISMATCH: session might be hijacked!')
+
+    const userData = await client.getUserInfo(code)
+
+    res.json(userData)
 })
 
 app.listen(3000, () => {
-	console.log("Server started on port 80")
+	console.log('Server started on port 3000')
 })
