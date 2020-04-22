14818c6784c7e9a67ab714a294501d21e1823de4
pacokwon <haechank@gmail.com>
2020-04-22 12:04:49 +0000

    add - body parser dependency

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
 	res.json({'a': 'b'})
